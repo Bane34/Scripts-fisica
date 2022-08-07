4211242987d9766a3ee6c5b685facd9905a62ca6
bane <bane@pop-os.localdomain>
2022-08-07 12:44:43 +0200

    Fixed names

--- gravitacion.py
+++ gravitacion.py
@@ -3,11 +3,8 @@ CONSTATE_G = 6.67e-11
 def fuerza_gravitatoria(m1, m2, radio):
     return CONSTATE_G * m1 * m2 / (radio ** 2)
 
-def fuerza_gravitatoria(campo, m):
+def fuerza_a_traves_de_campo(campo, m):
     return campo * m
 
 def campo(m, radio):
     return CONSTATE_G * m / (radio ** 2)
-
-if __name__ == '__main__':
-    print(campo(5.98e24, 6.37e6))
